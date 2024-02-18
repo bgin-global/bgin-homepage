acf0d2c6a107e205cec6138d38d453e9d1503947
masatsch <kingmasatojames@gmail.com>
2024-02-18 22:44:52 +0900

    next all

---
+++ contents/next-call.ts
@@ -0,0 +1,21 @@
+export const nextCalls = [
+  // IKP-WG
+  {
+    workingGroup: "Identity and Key Management WG",
+    thumbnail: "/images/Documents/IKPWG.svg",
+    nextCall: "2024/02/29 1AM UTC",
+    // LULU: Please fix it for zoom link or discourse link?
+    detailLink:
+      "https://docs.google.com/document/d/1S3C2dtJgpbnsD2T5hfP8Cq6wdxvW2ESUzUCo7Zzknv8/edit?usp=sharing",
+  },
+  // DTWG
+  {
+    workingGroup: "Decentralized Treasury WG",
+    thumbnail: "/images/Documents/DTWG.svg",
+    // LULU: Please fix it to exact date
+    nextCall: "Wed. 1PM UTC",
+    // LULU: Please fix it for zoom link or discourse link?
+    detailLink:
+      "https://docs.google.com/document/d/1Q11HpbiDi7v9Rt4qBDMV--XQhtGHks2k9T6foGp_ino/edit?usp=sharing",
+  },
+];
