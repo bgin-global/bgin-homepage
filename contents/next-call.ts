92a9e92376d3d675f8b869f02d4edaa4935305c5
Takaya9512 <150028494+Takaya9512@users.noreply.github.com>
2024-04-18 09:20:49 -0700

    Update next-call.ts

--- contents/next-call.ts
+++ contents/next-call.ts
@@ -1,19 +1,19 @@
 export const nextCalls = [
   // IKP-WG
   {
-    workingGroup: "Identity and Key Management WG",
+    workingGroup: "IAM, Key Management and Privacy (IKP) WG",
     thumbnail: "/images/Documents/IKPWG.svg",
-    nextCall: "2024/02/29 1AM UTC",
+    nextCall: "Thursday, 2024/04/25 11AM UTC",
     // LULU: Please fix it for zoom link or discourse link?
     detailLink:
       "https://docs.google.com/document/d/1S3C2dtJgpbnsD2T5hfP8Cq6wdxvW2ESUzUCo7Zzknv8/edit?usp=sharing",
   },
   // DTWG
   {
-    workingGroup: "Decentralized Treasury WG",
+    workingGroup: "Financial Applications & Social Economics (FASE) WG",
     thumbnail: "/images/Documents/DTWG.svg",
     // LULU: Please fix it to exact date
-    nextCall: "Wed. 1PM UTC",
+    nextCall: "Wednesday, 2024/04/24 11AM UTC",
     // LULU: Please fix it for zoom link or discourse link?
     detailLink:
       "https://docs.google.com/document/d/1Q11HpbiDi7v9Rt4qBDMV--XQhtGHks2k9T6foGp_ino/edit?usp=sharing",
