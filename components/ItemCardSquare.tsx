7b7e117ae417b17c6f7c5a715601def6eca85526
masatsch <kingmasatojames@gmail.com>
2024-09-12 16:25:34 +0900

    rounded

--- components/ItemCardSquare.tsx
+++ components/ItemCardSquare.tsx
@@ -2,6 +2,6 @@ interface Props {}
 
 export default function ItemCardSquare({}: Props) {
   return (
-    <div className="bg-[#002060] h-[260px] w-[241px] rounded shrink-0"></div>
+    <div className="bg-[#002060] h-[260px] w-[241px] rounded-xl shrink-0"></div>
   );
 }
