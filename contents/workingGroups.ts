7e70d0416888fe43b7f554338a235e97926b6565
masatsch <kingmasatojames@gmail.com>
2024-09-13 12:47:33 +0900

    wg links

--- contents/workingGroups.ts
+++ contents/workingGroups.ts
@@ -5,9 +5,9 @@ export interface WorkingGroup {
     job: string;
   }[];
   description: string;
-  mailinglist_url: string;
-  github_url: string;
-  detail_url: string;
+  subscribe_url: string;
+  document_url: string;
+  community_url: string;
   inactive: boolean;
 }
 
@@ -26,10 +26,10 @@ export const workingGroups: WorkingGroup[] = [
     ],
     description:
       "The goal of IAM, Key Management and Privacy (IKP) Working Group is to provide guidance and good practice documents that describe Identity (including keys) and Access management for access to crypto-currency exchange; Identity (including keys) and Access management using Blockchain/DLT for Online resource access Privacy considerations to be taken into account for the above.",
-    mailinglist_url: "https://bgin.discourse.group/c/ikp-wg/8",
-    github_url: "https://github.com/bgin-global",
-    detail_url:
+    subscribe_url: "",
+    document_url:
       "https://docs.google.com/document/d/1eRNE8hMW4X1ixzO3IowUWJU1q81YuO9fKT_MaRTZR5c/edit#heading=h.oj3duu3olyl0",
+    community_url: "https://bgin.discourse.group/c/ikp-wg/8",
     inactive: false,
   },
   {
@@ -47,10 +47,10 @@ export const workingGroups: WorkingGroup[] = [
     ],
     description:
       "The Financial Applications and Social Economics (FASE) working group focuses on an extensive array of topics around how blockchain technologies and crypto in general can enable new financial applications and socio economic growth.",
-    mailinglist_url: "https://bgin.discourse.group/c/working-group-s/fase/9",
-    github_url: "https://github.com/bgin-global",
-    detail_url:
+    subscribe_url: "https://github.com/bgin-global",
+    document_url:
       "https://drive.google.com/drive/folders/1h12WibGhcrHksuG1Cy9ULBJqJ7Kp7pi3",
+    community_url: "https://bgin.discourse.group/c/working-group-s/fase/9",
     inactive: false,
   },
   {
@@ -67,10 +67,10 @@ export const workingGroups: WorkingGroup[] = [
     ],
     description:
       "The Governance WG aims at developing the governance mechanism of BGIN itself. It will discuss a variety of topics including its organization structure, IPR policy and fundraising policy.",
-    mailinglist_url: "https://example.com",
-    github_url: "https://github.com/bgin-global",
-    detail_url:
+    subscribe_url: "",
+    document_url:
       "https://docs.google.com/document/d/1O641FrqZNX8IrgP5cKvAyyfBO80REEfBYCopXg5ieJ8/edit#heading=h.oj3duu3olyl0",
+    community_url: "",
     inactive: true,
   },
   {
@@ -83,9 +83,9 @@ export const workingGroups: WorkingGroup[] = [
     ],
     description:
       "The Decentralized Treasury WG aims at exploring best practices of raising funding that could/should replace ICO at all (as previous generations of fund raising were far from perfect) as well as ongoing fund/treasury management to finance operations/development of strategic initiatives.",
-    mailinglist_url: "https://bgin.discourse.group/",
-    github_url: "https://github.com/bgin-global",
-    detail_url: "https://example.com",
+    subscribe_url: "",
+    document_url: "",
+    community_url: "",
     inactive: true,
   },
 ];
