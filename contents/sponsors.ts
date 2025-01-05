17d640c6a7ccd5a6eb0de084da62050fc28f47d9
Takaya9512 <150028494+Takaya9512@users.noreply.github.com>
2025-01-05 18:30:16 -0500

    Update sponsors.ts

--- contents/sponsors.ts
+++ contents/sponsors.ts
@@ -1,14 +1,7 @@
 export const partnerships = [
   {
-    description:
-      "",
-    imageSrc: "",
-    imageAlt: "",
-  },
-  {
-    description:
-      "",
-    imageSrc: "",
-    imageAlt: "",
+    description: "Our partnership with the OpenWallet Foundation — a leading organization dedicated to developing open-source digital wallet solutions—propels our identity, access, and key management projects forward through dynamic collaboration and innovation.",
+    imageSrc: "/images/Sponsors/OpenWalletFoundation 1.svg",
+    imageAlt: "OpenWallet Foundation",
   },
 ];
