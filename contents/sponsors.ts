6500959c9dc6ba693e500dccd6256e7b49f772c1
Takaya9512 <150028494+Takaya9512@users.noreply.github.com>
2025-01-05 18:30:40 -0500

    Update sponsors.ts

--- contents/sponsors.ts
+++ contents/sponsors.ts
@@ -1,7 +1,7 @@
 export const partnerships = [
   {
-    description: "Our partnership with the OpenWallet Foundation — a leading organization dedicated to developing open-source digital wallet solutions—propels our identity, access, and key management projects forward through dynamic collaboration and innovation.",
-    imageSrc: "/images/Sponsors/OpenWalletFoundation 1.svg",
-    imageAlt: "OpenWallet Foundation",
+    description: "Partnering with ISO TC307—one of the premier standard- setting organizations in the blockchain space—accelerates our efforts in creating standards and best practices through its extensive expertise in developing international blockchain and distributed ledger standards.",
+    imageSrc: "/images/Sponsors/ISO_TC307 1.svg",
+    imageAlt: "ISO TC307",
   },
 ];
