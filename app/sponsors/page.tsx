52e3b84fe2de42e1b5e70d360b24d77c6632c164
Takaya9512 <150028494+Takaya9512@users.noreply.github.com>
2025-01-05 18:26:10 -0500

    Update page.tsx

--- app/sponsors/page.tsx
+++ app/sponsors/page.tsx
@@ -116,7 +116,7 @@ export default function Sponsors() {
        "Partnering with ISO TC307—one of the premier standard- setting organizations in the blockchain space—accelerates our efforts in creating standards and best practices through its extensive expertise in developing international blockchain and distributed ledger standards.",
        imageSrc: "/images/Sponsors/ISO_TC307 1.svg",
        imageAlt: "ISO TC307",
-      }
+      },
       ];
       <Footer />
     </main>
