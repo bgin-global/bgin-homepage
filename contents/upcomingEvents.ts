dd8e4a5ae4fe8de6b6c3e25fbf25df535471164f
haruki0409 <76884995+haruki0409@users.noreply.github.com>
2025-06-28 15:13:55 +0900

    Update upcomingEvents.ts (Test Commit)

--- contents/upcomingEvents.ts
+++ contents/upcomingEvents.ts
@@ -8,17 +8,17 @@ export const upcomingBlockConference = {
 export const upcomingLayer2Meetup = {
   title: "Layer2 Meetup@Sydney",
   date: "July 1-2",
-  location: "Switzerland,
+  location: "Switzerland",
   link: "/events/20250310-layer2-sydney",
 };
 
 export const upcomingWGCalls = [
   {
-    wgTitle: "GIKP WG",
-    date: "12pm Universal Time, June 5, 2025",
+    wgTitle: "IKP WG",
+    date: "12pm Universal Time, July 17, 2025",
   },
   {
     wgTitle: "FASE WG",
-    date: "11:30am Universal Time, June 4, 2025",
+    date: "11am Universal Time, July 2, 2025",
   },
 ];
